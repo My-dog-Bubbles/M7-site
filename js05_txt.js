00ba36a3b2de06df04c3ebb51c7e64a22428e80b
Angel Carmichael <amcarmichael2@my.waketech.edu>
2025-03-07 13:56:50 -0500

    positioning is off idk why

--- js05_txt.js
+++ js05_txt.js
@@ -4,11 +4,123 @@
       Chapter Case
 
       Application to generate a slide show
-      Author: 
-      Date:   
+      Author: Angel Carmichael
+      Date:   3/7/25
 
       Filename: js05.js
 */
+window.addEventListener("load", createLightbox)
+
+function createLightbox() {
+   // LightBox Container
+   let lightbox = document.getElementById("lightbox");
+
+   // Parts of the Lightbox
+   let lbTitle = document.createElement("hl");
+   let lbCounter = document.createElement("div");
+   let lbPrev = document.createElement("div");
+   let lbNext = document.createElement("div");
+   let lbPlay = document.createElement("div");
+   let lbImages = document.createElement("div");
+
+   //Design the lightbox title
+   lightbox.appendChild(lbTitle);
+   lbTitle.id = "lbTitle";
+   lbTitle.textContent = lightboxTitle;
+
+   //Design the lightbox slide counter
+   lightbox.appendChild(lbCounter);
+   lbCounter.id = "lbCounter";
+   let currentImg = 1;
+   lbCounter.textContent = currentImg + " / " +imgCount;
+
+   //Design the lightbox previous slide button
+   lightbox.appendChild(lbPrev);
+   lbPrev.id = "lbPrev";
+   lbPrev.innerHTML = "&#9664;";
+   lbPrev.onclick = showPrev;
+
+   //Design the lightbox next slide button
+   lightbox.appendChild(lbNext);
+   lbNext.id = "lbNext";
+   lbNext.innerHTML = "&#9654;";
+   lbNext.onclick = showNext;
+
+   //Design the lightbox play-pause button
+   lightbox.appendChild(lbPlay);
+   lbPlay.id = "lbPlay";
+   lbPlay.innerHTML = "&#9199;";
+   let timeID;
+   lbPlay.onclick = function(){
+      if (timeID) {
+         //stop the slidesShow
+         window.clearInterval(timeID);
+         timeID = undefined;
+      } else {
+         // start the slide show
+         showNext();
+         timeID = window.setInterval(showNext, 1500);
+      }   
+   }
+   
+   //Design the lightbox image container
+   lightbox.appendChild(lbImages);
+   lbImages.id = "lbImages";
+
+   // add images from the imgFiles array to the container
+   for (let i = 0; i < imgCount; i++){
+      let image = document.createElement("img")
+      image.src = imgFiles[i];
+      image.alt = imgCaptions[i];
+      image.onclick = createOverlay;
+      lbImages.appendChild(image);
+   }
+
+   // Function to move forward through the images
+   function showNext(){
+      lbImages.appendChild(lbImages.firstElementChild);
+      (currentImg < imgCount) ? currentImg++ : currentImg = 1;
+      lbCounter.textContent = currentImg + " / " + imgCount;
+   }
+
+   // Function to move backwards through the image list
+   function showPrev(){
+      lbImages.insertBefore(lbImages.lastElementChild, lbImages.firstElementChild);
+      (currentImg>1) ? currentImg-- : currentImg = imgCount;
+      lbCounter.textContent = currentImg + " / " + imgCount
+   }
+
+   function createOverlay (){
+      let overlay = document.createElement("div");
+      overlay.id = "lbOverlay";
+
+      // add the figure box to the overlay
+      let figureBox = document.createElement("figure");
+      overlay.appendChild(figureBox);
+      document.body.appendChild(overlay);
+
+      // add the image to the figure box
+      let overlayImage = this.cloneNode("true");
+      figureBox.appendChild(overlayImage);
+
+      // add the caption to the figure box
+      let overlayCaption = document.createElement("figcaption");
+      overlayCaption.textContent = this.alt
+      figureBox.appendChild(overlayCaption)
+
+      //ADD A CLOSE BUTTON TO THE OVERLAY
+      let closeBox = document.createElement("div");
+      closeBox.id = "lbOverlayClose";
+      closeBox.innerHTML = "&times;";
+      closeBox.onclick = function (){
+         document.body.removeChild(overlay);
+      }
+      overlay.appendChild(closeBox);
+      document.body.appendChild(overlay)
+   }
+
+   
+}
 
 window.addEventListener("load", setupGallery);
 
